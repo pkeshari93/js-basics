35b07cf6b4309e950fa849995f155506ce98eee6
Prakhar Keshari <prakhar.odyssey@gmail.com>
2018-02-03 18:43:11 -0600

    another one

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-alert('js linked');
+
+var num2 = '60';
+
+		// alert('my favorite num is' + num2);
+
+		var training = ['weight training', 'calisthenics', 'martial arts', 'powerlifting'];
+		// alert(colors[2]);
+
+		// another way for declaring arrays
+		var colors = new Array('red', 'yellow', 'blue');
+		colors[3] = 'magenta';
+
+		// dot syntax, calling a funtion on an object
+		colors.push('cool-blue');
+		// alert(colors);
+
+		// i can put a string with numbers in an array
+		var num = [1, 4, 3, 8, 5, 'six']
+		// alert(num.reverse());
+
+		for(var i = 0; i < 15; i++){
+			console.log(i);  
+		}
